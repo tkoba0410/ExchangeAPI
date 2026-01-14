da7845532b523c7fe4bb0670347a1e4df1e8b848
tkoba2 <tkoba2@nifty.com>
2026-01-14 20:49:15 +0900

    chore: add wire project and update references

---
+++ src/Wire/InternalsVisibleTo.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Exchange.Bitflyer.Raw")]
+[assembly: InternalsVisibleTo("Exchange.Bittrade.Raw")]
+[assembly: InternalsVisibleTo("Exchange.Bitflyer.Raw.Endpoints.Tests")]
+[assembly: InternalsVisibleTo("Exchange.Bitflyer.Raw.Tests")]
+[assembly: InternalsVisibleTo("Exchange.Bittrade.Raw.Endpoints.Tests")]
+[assembly: InternalsVisibleTo("Exchange.Bittrade.Raw.Tests")]

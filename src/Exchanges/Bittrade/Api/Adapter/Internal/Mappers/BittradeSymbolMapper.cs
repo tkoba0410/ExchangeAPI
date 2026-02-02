dd89783eb2446c826a1894d683638004df50af65
tkoba2 <tkoba2@nifty.com>
2026-02-02 18:50:39 +0900

    Add TryParse to BittradeSymbolMapper

--- src/Exchanges/Bittrade/Api/Adapter/Internal/Mappers/BittradeSymbolMapper.cs
+++ src/Exchanges/Bittrade/Api/Adapter/Internal/Mappers/BittradeSymbolMapper.cs
@@ -6,14 +6,26 @@ namespace ExchangeApi.Exchanges.Bittrade.Api.Adapter.Internal.Mappers;
 
 internal static class BittradeSymbolMapper
 {
-    public static Symbol ParseOrThrow(string symbol)
+    public static bool TryParse(string? symbol, out Symbol parsed)
     {
         if (string.IsNullOrWhiteSpace(symbol))
+        {
+            parsed = Symbol.Empty;
+            return false;
+        }
+
+        parsed = new Symbol(symbol);
+        return true;
+    }
+
+    public static Symbol ParseOrThrow(string symbol)
+    {
+        if (!TryParse(symbol, out var parsed))
         {
             throw new ArgumentException("symbol is required.", nameof(symbol));
         }
 
-        return new Symbol(symbol);
+        return parsed;
     }
 
     public static string ToProductCode(Symbol symbol)

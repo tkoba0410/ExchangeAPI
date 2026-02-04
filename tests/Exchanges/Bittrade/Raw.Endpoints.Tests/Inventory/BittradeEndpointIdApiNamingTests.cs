e3b1355b7fbe51ac868f3d8d6244a9f5a4ba9476
tkoba2 <tkoba2@nifty.com>
2026-02-04 16:10:30 +0900

    Add endpoint id naming checks

---
+++ tests/Exchanges/Bittrade/Raw.Endpoints.Tests/Inventory/BittradeEndpointIdApiNamingTests.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.IO;
+using ExchangeApi.Exchanges.Bittrade.Api.Raw.Api;
+using ExchangeApi.Exchanges.Bittrade.Api.Normalized.Api;
+using ExchangeApi.Tests.Inventory;
+
+namespace ExchangeApi.Tests.Exchanges.Bittrade.Raw.Endpoints.Tests;
+
+public sealed class BittradeEndpointIdApiNamingTests
+{
+    [Fact]
+    public void Bittrade_InventoryEndpointIds_MustHave_RawCallAsyncMethods()
+    {
+        var inventory = LoadInventoryEndpointIds();
+        EndpointIdNamingAssertions.AssertCallAsyncMethodsExist(inventory, typeof(IBittradeRawApi));
+    }
+
+    [Fact]
+    public void Bittrade_InventoryEndpointIds_MustHave_NormalizedCallAsyncMethods()
+    {
+        var inventory = LoadInventoryEndpointIds();
+        EndpointIdNamingAssertions.AssertCallAsyncMethodsExist(inventory, typeof(IBittradeNormalizedApi));
+    }
+
+    private static string InventoryFilePath =>
+        Path.Combine(InventoryEndpointIdParser.FindRepoRoot(), "docs", "inventory", "endpoints-bittrade.md");
+
+    private static IReadOnlyCollection<string> LoadInventoryEndpointIds() =>
+        InventoryEndpointIdParser.ParseEndpointIdsFromFile(InventoryFilePath);
+}
